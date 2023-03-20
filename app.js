ca02b12089ca978584956fae5b48e59f80738cef
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-03-19 21:28:00 -0500

    adding css and js

---
+++ app.js
@@ -0,0 +1,8 @@
+const screenDisplay = document.getElementsByClassName('screen');
+const buttons = document.querySelectorAll('buttons')
+
+function calculate(buttons) {
+    console.log(buttons);
+}
+
+calculate.addEventListener("click", (calculate) => )
